879507465460c6f1c26ffba9a834db6b89953f48
“matatabi” <senoo.h.gu@gmail.com>
2023-06-27 12:30:01 +0900

    ergf

--- main.js
+++ main.js
@@ -18,23 +18,32 @@ document.querySelector('.close-menu').addEventListener('click', () => {
 });
 
 window.addEventListener('scroll', function() {
+  // ヘッダー要素とカバー画像要素を取得します。
   let header = document.querySelector('.header');
   let coverImage = document.querySelector('.main-img');
+  // カバー画像の高さを取得します。
   let coverImageHeight = coverImage.offsetHeight;
+  // ロゴ画像要素を取得します。
   let logoImg = document.getElementById('logo-img');
 
+  // "white-header" クラスが body 要素に追加されているかどうかをチェックします。これは、このページでヘッダーの背景色を白にするかどうかを決定します。
+  if (document.body.classList.contains('white-header')) {
+    // "white-header" クラスが追加されている場合、色の変更はスキップされます（つまり、何もしません）。
+    return;  
+  }
+
+  // ユーザーがページをスクロールしたとき、カバー画像の高さを超えたらヘッダーの色を変更します。
   if (window.pageYOffset > coverImageHeight) {
     header.classList.add('colorized');
     logoImg.classList.add('colorized');
-    // ナビメニューの色を変更するコードを追加
     let navItems = document.querySelectorAll('.header-nav-menu-item a');
     navItems.forEach(function(item) {
       item.classList.add('colorized');
     });
   } else {
+    // スクロール位置がカバー画像の高さ未満の場合、ヘッダーの色を元に戻します。
     header.classList.remove('colorized');
     logoImg.classList.remove('colorized');
-    // ナビメニューの色を元に戻すコードを追加
     let navItems = document.querySelectorAll('.header-nav-menu-item a');
     navItems.forEach(function(item) {
       item.classList.remove('colorized');
